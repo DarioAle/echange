46b3a75ed23a89faf118614826f6f23ecbeae434
darioala <darioala@localhost.localdomain>
2021-02-25 21:50:10 -0600

    added identity screen copy pasted from previous project

---
+++ lib/Identity/identity.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class Identity extends StatefulWidget {
+  Identity({Key key}) : super(key: key);
+
+  @override
+  _IdentityState createState() => _IdentityState();
+}
+
+class _IdentityState extends State<Identity> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.5, 0.9],
+              colors: [
+                Color.fromRGBO(33, 66, 85, 1.0), 
+                Colors.black
+              ]
+          )
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(top: 50),
+                  child:
+                      Image(image: AssetImage('assets/images/logo_claro.png'))),
+              Row(children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 80),
+                    child: MaterialButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      height: 50,
+                      elevation: 2,
+                      color: Theme.of(context).buttonColor,
+                      child: Text(
+                        "REGISTRATE",
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () {
+                        print("register");
+                      },
+                    ),
+                  ),
+                ),
+              ]),
+              Row(children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 30),
+                    child: MaterialButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      height: 50,
+                      elevation: 2,
+                      color: Theme.of(context).buttonColor,
+                      child: Text(
+                        "INGRESA",
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () {
+                        print("there is something");
+                        // Navigator.of(context).push(
+                        //     MaterialPageRoute(builder: (context) => LogIn()));
+                      },
+                    ),
+                  ),
+                ),
+              ]),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// home
+const String APP_TITLE = "Snacks shop";
+// profile
+const String PROFILE_TITLE = "Perfil";
+const String PROFILE_LOGOUT = "Cerrar sesion";
+const String PROFILE_CART = "Lista de compras";
+const String PROFILE_WISHES = "Lista de deseos";
+const String PROFILE_HISTORY = "Historial de compras";
+const String PROFILE_SETTINGS = "Ajustes";
+const String PROFILE_NAME = "Pedro Paramo";
+const String PROFILE_EMAIL = "pp@decomala.com";
+const String PROFILE_PICTURE =
+    "https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+
+// app
+const PRIMARY_COLOR = Colors.indigo;
+const SECONDARY_COLOR = Colors.amber;
+const THIRD_COLOR = Colors.blueGrey;
+const BUTTON_COLOR = Colors.grey;
