1c13c8f3588212a9c9a754e27b73107710265725
chamedm <is708177@iteso.mx>
2021-03-20 19:52:21 -0600

    Changing home widget on main(replacing dev setting)

--- lib/main.dart
+++ lib/main.dart
@@ -11,22 +11,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: APP_TITLE,
-      home: Home(),
-      // home: new SplashScreen(
-      //   seconds: 5,
-      //   title: Text(
-      //     "E-change!",
-      //     style: TextStyle(color: Colors.white, fontSize: 50),
-      //   ),
-      //   image: Image(
-      //     image: AssetImage('assets/images/plant.png'),
-      //     height: 200,
-      //   ),
-      //   navigateAfterSeconds: Identity(),
-      //   photoSize: 200,
-      //   backgroundColor: Color.fromRGBO(129, 178, 154, 1.0),
-      // )
-    );
+        title: APP_TITLE,
+        // DEV-only
+        // home: Home(),
+        home: new SplashScreen(
+          seconds: 5,
+          title: Text(
+            "E-change!",
+            style: TextStyle(color: Colors.white, fontSize: 50),
+          ),
+          image: Image(
+            image: AssetImage('assets/images/plant.png'),
+            height: 200,
+          ),
+          navigateAfterSeconds: Identity(),
+          photoSize: 200,
+          backgroundColor: Color.fromRGBO(129, 178, 154, 1.0),
+        ));
   }
 }
